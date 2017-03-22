6026cbefb35157c75144b27d33a3e2f726f0449a
chelseadlee <chelsea.d.lee12@gmail.com>
2017-03-22 10:28:48 -0700

    catClicker updated with knockout

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-ud989-cat-clicker-ko-starter
-============================
+# CatClicker
 
-ud989-cat-clicker-ko-starter
+Simple web app built using knockout.js for Udacity Front End Web Development course.
+
+## Getting Started
+
+Download project files and open up index.html in any browser. Click on cat names to switch cats, and click on cats to increase clickcount. Increase your level by clicking on cats. Go from newborn to baby - child - teen - adult - middle aged - senior citizen.
+
+
--- js/app.js
+++ js/app.js
@@ -1,31 +1,87 @@
+var initialCats = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'http://lorempixel.com/640/480/cats/9',
+        nicknames: ['Mr. Kitty', 'Fluffikins', 'Scooot']
+    },
+    {
+        clickCount: 0,
+        name: 'Tiger',
+        imgSrc: 'http://lorempixel.com/640/480/cats/10',
+        nicknames: ['Muffin', 'Mr. Poops', 'Loafy']
+    },
+    {
+        clickCount: 0,
+        name: 'George',
+        imgSrc: 'http://lorempixel.com/640/480/cats/8',
+        nicknames: ['Snacky', 'Alexandar the Great', 'Potato']
+    },
+    {
+        clickCount: 0,
+        name: 'Scrappy',
+        imgSrc: 'http://lorempixel.com/640/480/cats/7',
+        nicknames: ['Poot', 'Marshmallow', 'Weirdo']
+    },
+    {
+        clickCount: 0,
+        name: 'Fuzz',
+        imgSrc: 'http://lorempixel.com/640/480/cats/5',
+        nicknames: ['Scruffy', 'Munchkin', 'Buzz']
+    }
+];
+
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
+
+    this.level = ko.computed(function() {
+        var level;
+        var clicks = this.clickCount();
+        if (clicks < 15) {
+            level = "newborn";
+        } else if (clicks < 30) {
+            level = "baby";
+        } else if (clicks < 45) {
+            level = "child";
+        } else if (clicks < 60) {
+            level = "teen";
+        } else if (clicks < 85) {
+            level = "adult";
+        } else if (clicks < 100) {
+            level = "middle aged";
+        } else if (clicks < 115) {
+            level = "senior citizen";
+        } else {
+            level = "ninja";
+        }
+        return level;
+    }, this);
+}
+
+
 var ViewModel = function() {
-    self = this;
-    self.clickCount = ko.observable(0);
-    self.name = ko.observable('Tabby');
-    self.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    self.imgAttribution = ko.observable('https://www.flickr.com/photos');
+    var self = this;
+
+    self.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat(catItem) );
+    });
+
+    self.currentCat = ko.observable( self.catList()[0] );
+
     self.incrementCounter = function() {
-        self.clickCount(self.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 
-    self.level = ko.computed(function() {
-        if (self.clickCount() < 15) {
-            return "newborn";
-        } else if (self.clickCount() < 30) {
-            return "baby";
-        } else if (self.clickCount() < 45) {
-            return "child";
-        } else if (self.clickCount() < 60) {
-            return "teen";
-        } else if (self.clickCount() < 85) {
-            return "adult";
-        } else if (self.clickCount() < 100) {
-            return "middle aged";
-        } else if (self.clickCount() >= 100) {
-            return "senior citizen";
-        }
-    }, self);
-
-}
+    self.setCurrentCat = function(clickedCat) {
+        self.currentCat(clickedCat);
+    };
+};
 
 ko.applyBindings(new ViewModel());
